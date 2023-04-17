c871158fd918491f8cd3fb80e26a2b6d0d6d33ca
oscardev-fe <oscarleon.alcatel@gmail.com>
2023-04-17 14:34:55 -0600

    testimoniales

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,7 +83,27 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiper = new Swiper(".testimonials_container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
 
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+
+        },
+
+    },
+});
 
 /*=============== INPUT ANIMATION ===============*/
 
